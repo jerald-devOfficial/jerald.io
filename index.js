78ae267dd081f6ceb908760324e9b73bb83e8f8a
Jerald Baroro <spaueOfficial@gmail.com>
2021-02-02 14:56:10 +0800

    Resetting the server for Blog fixes to Heroku

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 const express = require("express");
-const cors = require("cors");
-const keys = require("./config/keys");
-// const login = require("./routes/loginRoutes");
-const router = require("./routes/postRoutes");
 const app = express();
-app.use(cors());
+
 app.use(express.json());
-app.use("/api", router);
-// app.use("/auth", login);
 
 if (process.env.NODE_ENV === "production") {
   // Express will serve up production assets
@@ -22,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log("Successfully connected to the server.");
 });
