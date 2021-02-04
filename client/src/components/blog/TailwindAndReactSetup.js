4038161aa14ee47a1f28593dee41431a1e744f3c
Jerald Baroro <spaueOfficial@gmail.com>
2021-02-04 16:20:07 +0800

    Finished writing Guide to Complete TailwindCSS and React Setup blog

--- client/src/components/blog/TailwindAndReactSetup.js
+++ client/src/components/blog/TailwindAndReactSetup.js
@@ -287,7 +287,255 @@ function TailwindAndReactSetup() {
               will work but 'Adam Wathan', the creator of Tailwind CSS
               recommended using the full init of tailwindcss. The full
               tailwindcss config will compile all the classes from tailwindcss
-              itself. But it's really up to you this time.
+              itself. But it's really up to you this time. Now, this will create
+              both 'tailwind.config.js' full setup and 'postcss.config.js'.
+              Otherwise, it will only create a minimal 'tailwind.config.js'.
+            </p>
+            <hr className="pb-3" />
+            <h3 className="pb-6 text-1xl font-bold">
+              {content.blog.tailwindcssandreact.content[19]}
+            </h3>
+            <p className="pb-6">
+              {content.blog.tailwindcssandreact.content[20]}
+            </p>
+            <div class="mt-4 mb-10 rounded-xl overflow-x-auto bg-gray-800  text-green-700 py-2">
+              <pre class="language-js ">
+                <code class="language-diff-js">
+                  <span class="token unchanged language-json">
+                    <span class="token comment text-gray-500 pl-6">
+                      // package.json
+                    </span>
+                    <br />
+                    <span class="token punctuation pl-6">{"{"}</span>
+                    <br />
+                    {"   "}
+                    {"  "}"name": "my-app",
+                    <br />
+                    {"   "}
+                    {"  "}"version": "0.1.0",
+                    <br />
+                    {"   "}
+                    {"  "}"private": true,
+                    <br />
+                    {"   "}
+                    {"  "}"dependencies":{" "}
+                    <span class="token punctuation">{"{"}</span>
+                    <br />
+                    <div className="bg-red-300 w-full pl-6">
+                      <span class="token deleted-sign deleted language-js ">
+                        <span class="token prefix deleted select-none">-</span>
+                        {"  "}
+                        "autoprefixer": "^10.2.4",
+                        <br />
+                        <span class="token prefix deleted select-none">-</span>
+                        {"  "}
+                        "postcss": "^8.2.4",
+                        <br />
+                        <span class="token prefix deleted select-none">-</span>
+                        {"  "}
+                        "tailwindcss": "^2.0.2",
+                      </span>
+                    </div>
+                    <div className="bg-green-400 w-full pl-6">
+                      <span class="token inserted-sign inserted language-js">
+                        <span class="token prefix inserted select-none">+</span>
+                        {"  "}"autoprefixer": "^9.8.6",
+                        <br />
+                        <span class="token prefix inserted select-none">+</span>
+                        {"  "}"postcss": "^7.0.35",
+                        <br />
+                        <span class="token prefix inserted select-none">+</span>
+                        {"  "}"tailwindcss":
+                        "npm:@tailwindcss/postcss7-compat@^2.0.2",
+                        <br />
+                      </span>
+                    </div>
+                    <span class="token punctuation pl-6">
+                      {"   "}
+                      {"}"}
+                    </span>
+                    <br />
+                    <span class="token punctuation pl-6">{"}"}</span>
+                  </span>
+                </code>
+              </pre>
+            </div>
+            <p className="pb-6">
+              {content.blog.tailwindcssandreact.content[21]}
+            </p>
+            <p className="pb-6">
+              {content.blog.tailwindcssandreact.content[22]}
+            </p>
+            <LazyLoadImage
+              className="w-full object-cover lg:rounded pb-6"
+              src={content.blog.tailwindcssandreact.blogImages[3].img}
+              alt={content.blog.tailwindcssandreact.blogImages[3].alt}
+            />
+            <p className="pb-6">
+              {content.blog.tailwindcssandreact.content[23]}
+            </p>
+            <div class="mb-4 rounded-xl overflow-x-auto bg-gray-800 pl-2 py-2 text-green-700">
+              <pre class="language-shell">
+                <code class="language-shell">npm i</code>
+              </pre>
+            </div>
+            <p className="pb-6">
+              {content.blog.tailwindcssandreact.content[24]}
+            </p>
+            <div class="mb-4 rounded-xl overflow-x-auto bg-gray-800 pl-2 py-2 text-green-700">
+              <pre class="language-shell">
+                <code class="language-shell">npm run start</code>
+              </pre>
+            </div>
+            <hr className="pb-3" />
+            <h3 className="pb-6 text-1xl font-bold">
+              {content.blog.tailwindcssandreact.content[25]}
+            </h3>
+            <p className="pb-6">
+              {content.blog.tailwindcssandreact.content[26]}{" "}
+              <code className="text-green-700 bg-gray-800 rounded px-2 language-shell">
+                purge: ["./src/**/*.{"{(js, jsx, ts, tsx)}"}",
+                "./public/index.html"],
+              </code>{" "}
+              and it will look like this:
+            </p>
+            <div class="mt-4 mb-10 rounded-xl overflow-x-auto bg-gray-800  text-green-700 py-2">
+              <pre class="language-js ">
+                <code class="language-diff-js">
+                  <span class="token unchanged language-js pl-6">
+                    <span class="token prefix unchanged"> </span>{" "}
+                    <span class="token comment text-gray-500">
+                      // tailwind.config.js
+                    </span>
+                    <br />
+                    <span class="token prefix unchanged pl-6"> </span> module
+                    <span class="token punctuation">.</span>
+                    <span class="token property-access">exports</span>{" "}
+                    <span class="token operator">=</span>{" "}
+                    <span class="token punctuation">{"{"}</span>
+                    <br />
+                  </span>
+                  <div className="bg-red-300 w-full pl-6">
+                    <span class="token deleted-sign deleted language-js ">
+                      <span class="token prefix deleted select-none">-</span>{" "}
+                      {"  "}purge
+                      <span class="token operator">:</span>{" "}
+                      <span class="token punctuation">[</span>
+                      <span class="token punctuation">]</span>
+                      <span class="token punctuation">,</span>
+                      <br />
+                    </span>
+                  </div>
+                  <div className="bg-green-400 w-full pl-6">
+                    <span class="token inserted-sign inserted language-js">
+                      <span class="token prefix inserted select-none">+</span>{" "}
+                      {"  "}purge
+                      <span class="token operator">:</span>{" "}
+                      <span class="token punctuation">[</span>
+                      <span class="token string">
+                        './src/**/*.{"{js,jsx,ts,tsx}"}'
+                      </span>
+                      <span class="token punctuation">,</span>{" "}
+                      <span class="token string">'./public/index.html'</span>
+                      <span class="token punctuation">]</span>
+                      <span class="token punctuation">,</span>
+                      <br />
+                    </span>
+                  </div>
+                  <span class="token unchanged language-js">
+                    <span class="token prefix unchanged   pl-6"> </span> {"  "}
+                    darkMode
+                    <span class="token operator">:</span>{" "}
+                    <span class="token boolean">false</span>
+                    <span class="token punctuation">,</span>{" "}
+                    <span class="token comment text-gray-500">
+                      // or 'media' or 'class'
+                    </span>
+                    <br />
+                    <span class="token prefix unchanged  pl-6"> </span> {"  "}
+                    theme
+                    <span class="token operator">:</span>{" "}
+                    <span class="token punctuation">{"{"}</span>
+                    <br />
+                    <span class="token prefix unchanged  pl-6"> </span> {"    "}
+                    extend
+                    <span class="token operator">:</span>{" "}
+                    <span class="token punctuation">{"{"}</span>
+                    <span class="token punctuation">{"}"}</span>
+                    <span class="token punctuation">,</span>
+                    <br />
+                    <span class="token prefix unchanged  pl-6"> </span>{" "}
+                    <span class="token punctuation">
+                      {"  "}
+                      {"}"}
+                    </span>
+                    <span class="token punctuation">,</span>
+                    <br />
+                    <span class="token prefix unchanged  pl-6"> </span> {"  "}
+                    variants
+                    <span class="token operator">:</span>{" "}
+                    <span class="token punctuation">{"{"}</span>
+                    <br />
+                    <span class="token prefix unchanged  pl-6"> </span> {"    "}
+                    extend
+                    <span class="token operator">:</span>{" "}
+                    <span class="token punctuation">{"{"}</span>
+                    <span class="token punctuation">{"}"}</span>
+                    <span class="token punctuation">,</span>
+                    <br />
+                    <span class="token prefix unchanged  pl-6"> </span>{" "}
+                    <span class="token punctuation">
+                      {"  "}
+                      {"}"}
+                    </span>
+                    <span class="token punctuation">,</span>
+                    <br />
+                    <span class="token prefix unchanged  pl-6"> </span> {"  "}
+                    plugins
+                    <span class="token operator">:</span>{" "}
+                    <span class="token punctuation">[</span>
+                    <span class="token punctuation">]</span>
+                    <span class="token punctuation">,</span>
+                    <br />
+                    <span class="token prefix unchanged  pl-6"> </span>{" "}
+                    <span class="token punctuation">{"}"}</span>
+                  </span>
+                </code>
+              </pre>
+            </div>
+            <hr className="pb-3" />
+            <h3 className="pb-6 text-1xl font-bold">
+              {content.blog.tailwindcssandreact.content[27]}
+            </h3>
+            <p className="pb-6">
+              {content.blog.tailwindcssandreact.content[28]}
+            </p>
+            <div class="mt-4 mb-10 rounded-xl overflow-x-auto bg-gray-800  text-green-700 py-2 pl-6">
+              <pre class="language-css">
+                <code class="language-css">
+                  <span class="token comment text-gray-500">
+                    /* ./src/index.css */
+                  </span>
+                  <br />
+                  <span class="token atrule">
+                    <span class="token rule">@tailwind</span> base
+                    <span class="token punctuation">;</span>
+                  </span>
+                  <br />
+                  <span class="token atrule">
+                    <span class="token rule">@tailwind</span> components
+                    <span class="token punctuation">;</span>
+                  </span>
+                  <br />
+                  <span class="token atrule">
+                    <span class="token rule">@tailwind</span> utilities
+                    <span class="token punctuation">;</span>
+                  </span>
+                </code>
+              </pre>
+            </div>
+            <p className="pb-6">
+              {content.blog.tailwindcssandreact.content[29]}
             </p>
           </div>
           <Editor />
